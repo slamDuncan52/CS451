b0b9203aca46e24808af0d0a918038ed3e813533
Mitchell Duncan <dunc0474@kettering.edu>
2015-08-06 15:45:41 -0400

    Almost got background working, just need to pass it to child correctly.

--- lab02/shell.c
+++ lab02/shell.c
@@ -10,58 +10,45 @@ int main(void)
 {
         char* args[MAX_LINE / 2 + 1]; /* command line (of 80) has max of 40 arguments */
         char raw[MAX_LINE];
-        int should_run = 1;
+        char background = 0;
+        int should_run = 1, i, j, charRead;
 
-        int i, j, charRead, noWait;
-
-        while (should_run) 
-        {
+        while (should_run) {
                 i = 0;
                 j = 0;
                 charRead = 0;
-                noWait = 0;
+                background = 0;
                 fflush(stdout);
                 printf("mmsh--> ");
                 fgets(raw, MAX_LINE, stdin);
                 raw[strlen(raw) - 1] = '\0';
                 args[0] = strtok(raw, " ");
-                while (args[i] != NULL) 
-                {
-                        if (strcmp(args[i], "&") == 0) 
-                        {
-                                noWait = 1;
+                while (args[i] != NULL) {
+                        if (strcmp(args[i], "&") == 0) {
+                                background = 1;
                                 args[i] = NULL;
                         }
                         args[++i] = strtok(NULL, " ");
                 }
-                if (args[0] == NULL) 
-                {
+                if (args[0] == NULL) {
                         continue;
                 }
-                else if (strcmp(args[0], "exit") == 0) 
-                {
+                else if (strcmp(args[0], "exit") == 0) {
                         should_run = 0;
                         continue;
                 }
                 pid_t pid = fork();
-                if (pid == 0) 
-                {
+                if (pid == 0) {
+                        if (background == 1) {
+                                setpgid(0, 0);
+                        }
                         execvp(args[0], args);
                 }
-                else if (pid < 0) 
-                {
+                else if (pid < 0) {
                         exit(1);
                 }
-                else 
-                {
-                        if (noWait == 1) 
-                        {
-                                continue;
-                        }
-                        else 
-                        {
-                                wait(NULL);
-                        }
+                else {
+                        wait(NULL);
                 }
         }
         return 0;
