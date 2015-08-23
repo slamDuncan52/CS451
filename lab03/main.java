ea19b992b8494fee8d77c51de3e282d8df75bc33
Mikayla <MikaylaRay44@gmail.com>
2015-08-23 19:42:44 -0400

    Setup for lab 3

---
+++ lab03/main.java
@@ -0,0 +1,14 @@
+// Lab 3
+// Mitchell Duncan
+// Mikayla Ray
+
+
+
+/*  pp. 253 - 257
+
+	Producer Thread
+	Consumer Thread
+	Semaphores: Full, Empty, Mutex Lock
+	
+	Move items to and from a buffer synchronized with the 3 semaphores.
+*/
