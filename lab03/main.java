e17d30eec34586e413e8ada1ab552ed102ac1fdc
Mikayla <MikaylaRay44@gmail.com>
2015-08-24 15:53:50 -0400

    Retrieved acceptable command line arguments for program.

--- lab03/main.java
+++ lab03/main.java
@@ -1,14 +1,35 @@
-// Lab 3
-// Mitchell Duncan
-// Mikayla Ray
+// Lab 3 (Program 3 Chapter 5)
+// Producer - Consumer Problem
+// Mikayla Ray & Mitchell Duncan
 
-
-
-/*  pp. 253 - 257
-
-	Producer Thread
-	Consumer Thread
-	Semaphores: Full, Empty, Mutex Lock
+public static void main(String[] args) {
 	
-	Move items to and from a buffer synchronized with the 3 semaphores.
-*/
+	// Retrieve command line arguments
+	int sleepTime, numOfProducerThreads, numOfConsumerThreads;
+	
+	if (args[0] == null ||
+		args[0] < 0     ||
+		args[0] > Integer.MAX_VALUE) {
+		sleepTime = 0;
+	} else {
+		sleepTime = args[0];
+	}
+	
+	if (args[1] == null ||
+		args[1] < 0     ||
+		args[1] > Integer.MAX_VALUE) {
+		numOfProducerThreads = 0;
+	} else {
+		numOfProducerThreads = args[1];
+	}
+	
+	if (args[2] == null ||
+		args[2] < 0     ||
+		args[2] > Integer.MAX_VALUE) {
+		numOfProducerThreads = 0;
+	} else {
+		numOfProducerThreads = args[2];
+	}
+	
+	// Initialize the buffer
+}
